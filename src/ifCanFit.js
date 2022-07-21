9b6940cfab6514be7a0c6f5ccd931eefaa56fc70
OliverVK98 <v1155533@gmail.com>
2022-07-21 07:54:43 -0400

    Update #3.1

--- src/ifCanFit.js
+++ src/ifCanFit.js
@@ -2,6 +2,7 @@ function ifCanFit(element, length, shipArray) {
     let x = Number(element.className.slice(2, 3));
     let y = Number(element.className.slice(6, 7));
 
+    let answer;
     // if (shipArray.length != 0) {
     //     for (let i = 0; i < shipArray.length; i++) {
     //         if (y === shipArray[i].yCord) {
@@ -15,18 +16,20 @@ function ifCanFit(element, length, shipArray) {
 
     if (shipArray.length != 0) {
         shipArray.forEach(ship => {
-            if (ship.yCord === y) {
+            if (y === ship.yCord) {
                 for (let i = 0; i < length; i++) {
-                    console.log(ship.xCord.includes(x + i));
-                    if (ship.xCord.includes(x + i)) return false;
+                    // console.log(ship.xCord.includes(x + i));
+                    if (ship.xCord.includes(x + i)) answer = false;
                 }
             }
         });
     }
 
 
-    if (x < (10 - length)) return true;
-    return false;
+    if (x < (10 - length)) answer = true
+    else answer = false;
+    console.log(answer);
+    return answer;
 }
 
 export default ifCanFit
